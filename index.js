ed186a22cd2623fa074702774913977539e4827c
Praga Siva <praga@studentedge.com.au>
2018-08-05 20:28:39 +0800

    Add options

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const wkhtmltopdf = require("./utils/wkhtmltopdf");
 const errorUtil = require("./utils/error");
 
 exports.handler = function handler(event, context, callback) {
-    if (!event.html) {
-        const errorResponse = errorUtil.createErrorResponse(400, "Validation error: Missing field 'html'.");
+    if (!event.html_base64) {
+        const errorResponse = errorUtil.createErrorResponse(400, "Validation error: Missing field 'html_base64'.");
 
         callback(errorResponse);
         return;
     }
+	
+	var html_utf8 = new Buffer(event.html_base64, 'base64').toString('utf8');
 
-    wkhtmltopdf(event.html)
+    wkhtmltopdf(html_utf8, event.options)
         .then(buffer => {
             callback(null, {
                 data: buffer.toString("base64")
